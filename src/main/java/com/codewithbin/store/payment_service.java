5ead07c8b5350948e701a42c4ee185aa77702ddb
NguyenVanBin_2151012001 <bin2003mail@gmail.com>
2025-05-11 14:54:12 +0700

    clear repo

--- src/main/java/com/codewithbin/store/payment_service.java
+++
@@ -1,5 +0,0 @@
-package com.codewithbin.store;
-
-public interface payment_service {
-    void processPayment(double amount);
-}
--- src/main/java/com/codewithbin/store/stripe_payment_service.java
+++
@@ -1,9 +0,0 @@
-package com.codewithbin.store;
-
-public class stripe_payment_service implements payment_service {
-    @Override
-    public void processPayment(double amount) {
-        System.out.println("stripe");
-        System.out.println("Amound="+amount);
-    }
-}
